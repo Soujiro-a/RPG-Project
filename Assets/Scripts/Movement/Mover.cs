d55366cc925a20752945a153deb8b4e27a7d0056
Soujiro <qmaecx@naver.com>
2021-02-24 21:16:03 +0900

    02/24 21:15 플레이어 행동 추가
    메인 카메라는 플레이어 추적, 지면 클릭 시 플레이어가 움직임.

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Ray lastRay;
+    Animator anim;
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+        UpdateAnimator();
+    }
+
+    void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            agent.destination = hit.point;
+        }
+    }
+
+    void UpdateAnimator()
+    {
+        Vector3 velocity = agent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        anim.SetFloat("forwardSpeed", speed);
+    }
+}
